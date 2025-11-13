5d58e630e0b90960df9d2e7281e74c537bffbd1d
JiHyun Jang <gihyun0818@gmail.com>
2025-11-13 20:10:21 +0900

    대용량 엑셀 다운로드 예제 시작

---
+++ src/main/java/io/github/jihyundev/excel_download/ExcelDownloadApplication.java
@@ -0,0 +1,13 @@
+package io.github.jihyundev.excel_download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jihyundev/excel_download/ExcelDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jihyundev.excel_download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
